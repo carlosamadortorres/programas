8bb86509707009a8ed82fe1a8e73bf9f9d357902
carlosamadortorres <carlosamadoruber@gmail.com>
2022-10-09 19:35:25 -0700

    mi primer commit

---
+++ miprimer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+   <head>
+     <meta charset="utf-8">
+   <title> Mi primer html</title>
+   </head>
+
+   <body>
+       <p> A UN PASO DE CONVERTIRME EN HENRY</p>
+
+       <?php echo 'Hola mundo   Soy novato';
+
+             echo 'Espero que lo haga mejor';
+       ?>
+   </body>
+
+
+</html>
